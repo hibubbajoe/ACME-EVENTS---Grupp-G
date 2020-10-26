ec606d4abbef4f9bd02fc7bf5f3076b02bbf7281
Sara Viktorsson <saraviktorsson@hotmail.com>
2020-10-26 14:37:39 +0100

    layour för adminsidan finns nu, men är ej helt klar

--- admin.js
+++ admin.js
@@ -42,3 +42,47 @@ deleteButton.addEventListener("click",function(){
 
 })
 */
+ /*-----sara lagt till: -------------*/
+event_obj = JSON.parse(localStorage.getItem("event_array"));
+let crud_list = document.getElementById("crud-list");
+
+        for (let event of event_obj) {
+            let row = document.createElement("tr");
+            row.classList.add("crud-item")
+            let td_date = document.createElement("td");
+            let td_name = document.createElement("td");
+            let td_location = document.createElement("td");
+            let td_avaliable = document.createElement("td");
+            let td_edit = document.createElement("button");
+            let td_delete = document.createElement("button");
+            td_date.innerText = event.date;
+            td_name.innerText = event.name;
+            td_location.innerText = event.location;
+            td_edit.innerText = "Edit"
+            td_delete.innerText = "Delete"
+            
+
+            crud_list.append(row);
+            row.append(td_date);
+            row.append(td_name);
+            row.append(td_location);
+            
+
+            if (!event.isAvaliable) {
+                td_avaliable.innerText = "cancelled"
+                td_avaliable.classList.add("cancelled");
+                row.append(td_avaliable);
+                //hämta class som gör texten röd
+            } else {
+                let more_info_btn = document.createElement("button");
+                more_info_btn.innerText = "More info";
+                more_info_btn.setAttribute("onclick", "window.location.href='eventdetails.html';")
+                td_avaliable.append(more_info_btn);
+                row.append(td_avaliable);
+            }
+
+            row.append(td_edit);
+            td_edit.classList.add("edit");
+            row.append(td_delete);
+            td_delete.classList.add("delete");
+        }
