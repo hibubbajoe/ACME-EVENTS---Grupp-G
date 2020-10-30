28c68487102e2949c87fe9bc13ff5e2205342366
Sara Viktorsson <saraviktorsson@hotmail.com>
2020-10-30 08:07:07 +0100

    removed some code that is not being used, and wrote comments in english

--- eventlist.js
+++ eventlist.js
@@ -1,6 +1,7 @@
 class EventList {
     constructor() {
-        this.eventArray = []; // array som ska innehålla alla eventobjekt. 
+        // this array will contain all the event objects
+        this.eventArray = []; 
         this.filter_btn = document.getElementById("category");
         this.filter_btn.addEventListener("change", () => {
             this.filterByCategory();
@@ -11,19 +12,18 @@ class EventList {
         })
         this.admin_btn = document.getElementById("admin_btn");
         this.admin_btn.addEventListener("click", () => {
-            //visa formuläret
             this.displayAdmin();
         })
-        let create_btn = document.getElementById("create_btn");
-        create_btn.addEventListener("click", () => {
+        this.create_btn = document.getElementById("create_btn");
+        this.create_btn.addEventListener("click", () => {
             this.create();
         })
     }
+
+    // this will show all forms and buttons the admin can use to CRUD the list
     displayAdmin() {
         let add_form = document.getElementById("add_form");
         add_form.classList.toggle("hidden");
-
-        //tar bort klass hidden på deleteknapparna
         
         let delete_btns = document.querySelectorAll(".delete_btn");
         delete_btns.forEach(function(item){
@@ -34,33 +34,35 @@ class EventList {
         edit_btns.forEach(function(item){
             item.classList.toggle("hidden");
         })    
-
     }
-    // lägg in att knapparna ska visas döljar här så det slipper stå på så många ställen i koden
+    
+    // TODO:
+    // The edit and delete buttons are toggled all around in the code so we should have a method for that.
     toggleButtons() {
 
     }
-    // CRUD - create
+    // CRUD - create. This runs when the "add" button is clicked
     create() {
-        //eventlisteners på alla inputs och knapp
-        //skapa nyt event av detta.
+        // get all input fields from the "add form"
         let date_input = document.getElementById("event_date");
         let name_input = document.getElementById("name");
         let location_input = document.getElementById("location");
         let category_input = document.getElementById("event_category");
         let isAvaliable_input = document.getElementById("isAvaliable");
-        
+        // create a new event object from the users inputs
             let date = date_input.value;
             let name = name_input.value;
             let location = location_input.value;
             let category = category_input.value;
             let isAvaliable = isAvaliable_input.value;
-            let new_event = new Events(date, name, location, category, isAvaliable);
+            let new_event = new Event(date, name, location, category, isAvaliable);
             eventlist.addEvent(new_event);
             eventlist.storeEvent(new_event);
             eventlist.clearTable();
             eventlist.printEvent();
 
+            // since the list will be printed out again all the admin buttons will be hidden. We dont want that
+            // this code below is repeated many times so it will be moved to its own method later. 
             let delete_btns = document.querySelectorAll(".delete_btn");
             delete_btns.forEach(function(item){
             console.log(item);
@@ -70,32 +72,21 @@ class EventList {
             edit_btns.forEach(function(item){
             console.log(item);
             item.classList.remove("hidden");
+            })
         })
-            
-        })
-        console.log(this.eventArray);
-            
     }
 
     addEvent(event) {
         this.eventArray.push(event);
     };
-    /*
-    deleteEvent(index, amount) {
-        this.eventArray.splice(index, amount);
-    };
-    */
 
-   // CRUD - delete
+   // CRUD - delete. This happens when any deletebutton is klicked
     delete() {
+        //Finds the right object with target and removes that objects from the event array
         let index = event.target.id;
-        console.log(index);
-        //let delete_btn = event.target;
-        //console.log(delete_btn)
-        console.log("event deleted");
         this.eventArray.splice(index, 1);
-        console.log(this.eventArray);
-        // skriv över event_array i local storage:
+    
+        // The altered array will overwrite the old one.
         this.storeEvent();
         this.clearTable();
         this.printEvent();
@@ -111,15 +102,21 @@ class EventList {
         })
         
     }
-    //CRUD - edit
+    //CRUD - edit. This will happen when any edit button is klicked. 
     edit() {
+
+        //disables the buttons to prevent the bugg that happens when the user klicks "admin" when the edit form is open
+        this.admin_btn.disabled = true;
+        this.create_btn.disabled = true;
+
+        // The edit form will now be shown
         let edit_form = document.getElementById("edit_form");
         edit_form.classList.toggle("hidden");
 
+        //finds the right object with target
         let index = event.target.id;
-        //console.log(this.eventArray[index].date);
-        //console.log(this.eventArray[index]);
-        //formuläret aka få de värden som objektet har just nu:
+    
+        //When the form is shown it will have the current values of the objects filled in.
         let input_date = document.getElementById("edit_date");
         input_date.value = this.eventArray[index].date;
         let input_name = document.getElementById("edit_name");
@@ -134,18 +131,17 @@ class EventList {
         let save_btn = document.getElementById("save_btn");
             save_btn.addEventListener("click", () => {
                 
-                console.log(index);
-                // när save trycks ska det sparas
+                // when save button is clicked the values till be saved in the object
                 this.eventArray[index].date = input_date.value;
                 this.eventArray[index].name = input_name.value;
                 this.eventArray[index].location = input_location.value;
                 this.eventArray[index].category = input_category.value;
                 this.eventArray[index].isAvaliable = input_isAvaliable.value;
-                console.log(this.eventArray[index]);
-                // uppdatera local storage 
+            
+                // This will remove the current target so that when another edit button is klicked ONLY that one will be targeted (bugg)
                 index = null;
-                this.storeEvent(this.eventArray);
-                //uppdatera listan på sidan
+
+                this.storeEvent();
                 this.clearTable();
                 this.printEvent();
 
@@ -158,68 +154,61 @@ class EventList {
                 edit_btns.forEach(function(item){
                 item.classList.toggle("hidden");
                 })  
+
+                // when save is clicked the edit form should be hidden again
                 edit_form.classList.toggle("hidden");
+
+                // after "save" is clicked the buttons will work again
+                this.admin_btn.disabled = false;
+                this.create_btn.disabled = false;
                 
             })
     };
 
-    //sparar array av eventobjekten i local storage 
+    //saves the array of event objects to local storage as "event_array"
     storeEvent() {
         let event_string = JSON.stringify(this.eventArray);
         localStorage.setItem("event_array", event_string);
     }
    
-    //hämtar data från local storage och skriv ut som lista.
-    printEvent(key) {
+    //get the data from local storage and prints it out on the web page.
+    printEvent() {
 
-        let event_obj;
-
-        if (key === "sorted") {
-            this.clearTable();
-            event_obj = JSON.parse(localStorage.getItem("sorted_array"));
-            //annars hämtas "event_array" och hela listan skrivs.
-        } else {
-            event_obj = JSON.parse(localStorage.getItem("event_array"));
-            console.log("Array med alla objekt:")
-            //console.log(event_obj);
-        }
-
-        console.log(event_obj)
+        let event_obj = JSON.parse(localStorage.getItem("event_array"));
 
         let list = document.getElementById("event-list");
 
-        //for (let event of event_obj) {
             event_obj.forEach(function(event, index){
 
-            
-            let row = document.createElement("tr");
-            row.classList.add("event-item")
-            let td_date = document.createElement("td");
-            let td_name = document.createElement("td");
-            let td_location = document.createElement("td");
-            let td_avaliable = document.createElement("td");
-            td_date.innerText = event.date;
-            td_name.innerText = event.name;
-            td_location.innerText = event.location;
+                let row = document.createElement("tr");
+                row.classList.add("event-item")
+                let td_date = document.createElement("td");
+                let td_name = document.createElement("td");
+                let td_location = document.createElement("td");
+                let td_avaliable = document.createElement("td");
+                td_date.innerText = event.date;
+                td_name.innerText = event.name;
+                td_location.innerText = event.location;
 
-            list.append(row);
-            row.append(td_date);
-            row.append(td_name);
-            row.append(td_location);
+                list.append(row);
+                row.append(td_date);
+                row.append(td_name);
+                row.append(td_location);
 
-            if (!event.isAvaliable) {
-                td_avaliable.innerText = "cancelled"
-                td_avaliable.classList.add("cancelled");
-                row.append(td_avaliable);
-                //hämta class som gör texten röd
-            } else {
-                let more_info_btn = document.createElement("button");
-                more_info_btn.innerText = "More info";
-                more_info_btn.setAttribute("onclick", "window.location.href='eventdetails.html';")
-                td_avaliable.append(more_info_btn);
-                row.append(td_avaliable);
+                // if true the event will have a "more info button" if false there will be a "cancelled" text
+                // TODO: fix this bugg in the admin form: right now it will always be true if there is any input and false if there is no input
+                if (!event.isAvaliable) {
+                    td_avaliable.innerText = "cancelled"
+                    td_avaliable.classList.add("cancelled");
+                    row.append(td_avaliable);
+                } else {
+                    let more_info_btn = document.createElement("button");
+                    more_info_btn.innerText = "More info";
+                    more_info_btn.setAttribute("onclick", "window.location.href='eventdetails.html';")
+                    td_avaliable.append(more_info_btn);
+                    row.append(td_avaliable);
             }
-            //deleteknapp
+            //Every event has a delete button and an edit butten, but they are hidden until the admin button is klicked. 
             let td_delete = document.createElement("td");
             let delete_btn = document.createElement("button")
             delete_btn.innerText = "Delete";
@@ -232,7 +221,6 @@ class EventList {
             td_delete.append(delete_btn);
             row.append(td_delete);
 
-            //editknapp
             let td_edit = document.createElement("td");
             let edit_btn = document.createElement("button")
             edit_btn.innerText = "Edit";
@@ -245,22 +233,22 @@ class EventList {
             td_edit.append(edit_btn);
             row.append(td_edit);
         })
-        //}
+        
     }
 
-    //filter function to only show selected category
+    //filter method to only show selected category
     filterByCategory() {
 
-        //to access all events on display
+        //gets all events on display
         let event_row = document.getElementsByClassName("event-item")
 
-        //making the if and foor loops look a little better
+        // finds the selected category from the drop down menu
         let category = this.filter_btn.options[this.filter_btn.selectedIndex].text;
 
         //get event-array from local storage
         let event_array = JSON.parse(localStorage.getItem("event_array"));
 
-        //finding what category is selected
+        //filtering out only the objects with the selected category from the event_array
         let filtered_array = event_array.filter((obj) => {
             return obj.category === category;
         });
@@ -272,12 +260,12 @@ class EventList {
                     ? event_row[i].classList.remove("hidden")
                     : event_row[i].classList.add("hidden");
             }
-        } else { //if category is selected the whole board is displayed 
+        } else { //if "Category" is selected all the events will be displayd again
             for (let i = 0; i < event_array.length; i++) {
                 event_row[i].classList.remove("hidden")
             }
         }
-        //spara till local storage
+        //save the filtered array to local storage as "filtered_array"
         let event_string = JSON.stringify(filtered_array);
         localStorage.setItem("filtered_array", event_string);
     }
@@ -288,7 +276,7 @@ class EventList {
         let sortByDate = document.getElementById("date");
         let sorted_array = [];
 
-        //loopa igenom objects i local storage och sortera dom utefter datum
+        //loop though objects in locak storage and sort them by date
         let event_array = JSON.parse(localStorage.getItem("event_array"));
 
         if (sortByDate.selectedIndex === 0) {
@@ -301,54 +289,39 @@ class EventList {
             })
         }
 
-        //spara till local storage
+        //the sorted array will overwrite the "event_array" in local storage
         let event_string = JSON.stringify(sorted_array);
         localStorage.setItem("event_array", event_string)
 
         this.clearTable();
-        this.printEvent();
-
-        let delete_btns = document.querySelectorAll(".delete_btn");
-        delete_btns.forEach(function(item){
-            console.log(item);
-            item.classList.remove("hidden");
-        })
-        let edit_btns = document.querySelectorAll(".edit_btn");
-        edit_btns.forEach(function(item){
-            item.classList.remove("hidden");
-        })
-
-        
+        this.printEvent();      
     }
-
+    // This will just remove all events from the list when needed
     clearTable() {
-        let events = document.querySelectorAll("tr");
-        events.forEach(function (event) {
+        let Event = document.querySelectorAll("tr");
+        Event.forEach(function (event) {
             event.remove();
         })
     }
 }
 
-class Events {
-    constructor(date, name, location, category, isAvaliable) { // eventuellt kommer varje objekt behöva ett id.
-        this.date = date; // hur ska vi hantera datum för att det ska gå att sortera sedan?
+class Event {
+    constructor(date, name, location, category, isAvaliable) { 
+        this.date = date; 
         this.name = name;
         this.location = location;
         this.category = category;
-        this.isAvaliable = isAvaliable; // tänker att om det är true betyder det att det kommer vara en knapp här som tar en till detaljsidan
-        //men om  det är false så kommer det istället stå cancelled. 
+        this.isAvaliable = isAvaliable; 
     }
 }
 
+// Some events are on the page when its loaded:
 
-
-// Några Eventobjekt som ska finnas från början när sidan har laddats.
-
-let event1 = new Events("July 3, 22:00", "Anthony Jeselnik", "Civic Auditorium", "Stand Up", false)
-let event2 = new Events("July 7, 20:00", "James Blake", "L'Olympia", "Music", true)
-let event3 = new Events("August 13, 23:00", "Nina Simone", "Fasching", "Music", false)
-let event4 = new Events("August 17, 13:00", "PSG vs Milan", "Le Parc des Princes", "Sport", false)
-let event5 = new Events("August 17, 13:00", "PSG vs Milan", "Le Parc des Princes", "Sport", false)
+let event1 = new Event("July 3, 22:00", "Anthony Jeselnik", "Civic Auditorium", "Stand Up", false)
+let event2 = new Event("July 7, 20:00", "James Blake", "L'Olympia", "Music", true)
+let event3 = new Event("August 13, 23:00", "Nina Simone", "Fasching", "Music", false)
+let event4 = new Event("August 17, 13:00", "PSG vs Milan", "Le Parc des Princes", "Sport", false)
+let event5 = new Event("August 17, 13:00", "PSG vs Milan", "Le Parc des Princes", "Sport", false)
 
 let eventlist = new EventList;
 
@@ -357,6 +330,7 @@ eventlist.addEvent(event2);
 eventlist.addEvent(event3);
 eventlist.addEvent(event4);
 
+// comment out these if you dont want the list to be over written every time the page loads. 
 eventlist.storeEvent(event1);
 eventlist.storeEvent(event2);
 eventlist.storeEvent(event3);
